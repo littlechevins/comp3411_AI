33763dcab5d761a58a2f6ca529e33ef46f086277
Shiwei Sun <33053669+Flying-Apple@users.noreply.github.com>
2018-06-01 17:34:53 +1000

    Update reachbility.py

--- ass3/src/reachbility.py
+++ ass3/src/reachbility.py
@@ -1,4 +1,22 @@
+def movetodoor(Map,DoorLocation):  #call
 
+    current = (Map.locX, Map.locY)
+
+    movelist = a_star_search(current,DoorLocation)
+
+    #append turn to door + U(unlock) to movelist
+
+    return(coord2Action(movelist))
+
+def movetotree(Map,TreeLocation):
+
+    current = (Map.locX, Map.locY)
+
+    movelist = a_star_search(current,TreeLocation)
+
+    #append turn to tree + C(chop) to movelist
+
+    return(coord2Action(movelist))
   
 def isTilePassable(tile,hasKey,hasAxe,stone):  ###stone is the number of stone agent has
     if (tile == '~' and stone>0):
